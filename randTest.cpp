5dde692fb59aad85a9e2f1bffbf7bde54d93d18a
freemanmtc <freemanmtc@gmail.com>
2013-01-14 10:32:17 -0200

    added cin

--- randTest.cpp
+++ randTest.cpp
@@ -6,15 +6,20 @@ int main(){
 
 	srand(time(0));
 
-	int r0=rand();
+	int r0=rand(),rangeA,rangeB;
 
 	/* a + rand() % ((b + 1) -a) */
-	int rangeA=5;
-	int rangeB=15;
+	std::cout << "plz input the int for the start range: ";
+	std::cin >> rangeA;
+	std::cout << "start range value: " << rangeA << std::endl;
+	std::cout << "plz input the int for the end range: ";
+	std::cin >> rangeB;
+	std::cout << "end range value: " << rangeB << std::endl;
+
 	int r1= rangeA + rand() % ((rangeB + 1) - rangeA);
 
 	std::cout << "rand with no range: " << r0 << std::endl;
-	std::cout << "rand with range [" << rangeA << "," << rangeB << "] :" << r1 << std::endl;
+	std::cout << "rand with range [" << rangeA << "," << rangeB << "]: " << r1 << std::endl;
 
 	return 0;
 }
