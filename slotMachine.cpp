0335b5cf380ce96fbf3d79ae2d63d6fa57a1ec2f
freemanmtc <freeman@localhost.(none)>
2013-06-26 21:18:03 -0300

    changed the slot machine to his propose,

--- slotMachine.cpp
+++ slotMachine.cpp
@@ -1,10 +1,52 @@
 /* slot machine test */
+/*
+ * create a slot machine, the program sohould start the playor off with 1000.00 bucks
+ *
+ * how a slot machine works
+ * after the player inout his bet. the promagram must calculate 3 random numbers in the range 2,7 and output them neatly on the screen.
+ *
+ * if all three numbers are 7, the the player wins ten times their betting money.
+ * if the three numbers are all the same, but not sevens, the player win five times
+ * if two out of the tree numbers are the same, then the player wins three times
+ * else the player looses his bet
+ */
+
+// todo: fix the bug from cin fail and switch default
 #include <iostream>
 
 int main(){
 
-	int money=1000;	
+	int betMoney=1000;
+//	bool continue=false;
+	int choice;
 	
+//	std::cout << betMoney;
+	std::cout << "#### Welcome to freemanmtc' Slot Machine ! ##### \n## Have Fun ##\n";
+
+	while(true){
+		std::cout << "what you want to do? \n(1)Play (2)Go home\n";
+		std::cin >> choice;
+
+		while(std::cin.fail())
+		{
+			std::cout << "you have just two options here man.\n";
+			std::cin.clear();
+			std::cin.ignore(10,'\n');
+		}
+			
+		switch(choice){
+			case 1:
+				std::cout << "so, lets start the game!!!\n";
+				break;
+			case 2:
+				std::cout << "bye bye !\n";
+				return 0;
+				break;
+			default: std::cout << "you have just two options here man\n";
+				break;
+		}
+	}
+	std::cin.get();
 	return 0;
 
 }
